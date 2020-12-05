9121ed9b5d250df7195ca15cfe1f7ec87d79631f
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-05 15:16:52 -0600

    refactored return statement to fix error

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import { ApplicationViews } from './ApplicationViews'
 
-export const KandyKorner = () => {
+export const KandyKorner = () => (
     <>
         <Route render={() => {
-            return (
+            return ( <>
                 <Route render={props => <ApplicationViews {...props} /> } />
+            </>
             )
         }} />
     </>
-}
+    )
