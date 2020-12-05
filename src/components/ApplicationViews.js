29bdac1038e051417e0315c42a1f6c5a2789fb3f
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-05 16:20:07 -0600

    added ProductList and ProductProvider, refactored Route to be dry

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,17 +1,44 @@
-import React from "react"
-import { LocationList } from "./location/LocationList"
-import { LocationProvider } from "./location/LocationProvider"
+import React from "react";
+import { LocationList } from "./location/LocationList";
+import { LocationProvider } from "./location/LocationProvider";
 import { Route } from "react-router-dom";
-
+import { ProductProvider } from "./products/ProductProvider";
+import { ProductList } from "./products/ProductList";
 
 export const ApplicationViews = (props) => {
-    return (
-        <>
-            <LocationProvider>
-                <Route exact path="/locations"
-                render={(props) => <LocationList {...props} />}
-                />
-            </LocationProvider>
-        </>
-    )
-}
+  return (
+    <>
+      <LocationProvider>
+        <ProductProvider>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <>
+                <LocationList />
+                <ProductList {...props} />
+              </>
+            )}
+          />
+        </ProductProvider>
+      </LocationProvider>
+      ;
+    </>
+  );
+};
+
+//This is the same as above, but not as dry as it uses Route twice for the same path
+/* <LocationProvider>
+        <ProductProvider>
+          <Route
+            exact
+            path="/"
+            render={(props) => <LocationList {...props} />}
+          />
+          <Route
+            exact
+            path="/"
+            render={(props) => <ProductList {...props} />}
+          />
+        </ProductProvider>
+      </LocationProvider> */
