8c35b3800e9d217ef9aadf3f007fe831b82dc66a
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-05 16:20:48 -0600

    added Product funtion to render name, price, and producType

---
+++ src/components/products/Product.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const Product = ({ product }) => (
+    <section className="product">
+        <h3 className="product__name">{product.name}</h3>
+        <div className="product__price">${product.price} each</div>
+        <div className="product__productType">Type: {product.productType}</div>
+    </section>
+)
