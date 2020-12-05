c16a056e4def6155590e095a137d19ac4e9e588c
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-05 15:17:52 -0600

    reactored return statement to mao over location data

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -1,12 +1,20 @@
 import React, { useContext, useEffect } from "react"
-import { locationContext } from './LocationProvider'
+import { LocationContext } from './LocationProvider'
+import { Location } from './Location'
 
 //create LocationList function to get the array of locations from the location provider
 export const LocationList = () => {
-    const { locations, getLocations } = useContext(locationContext)
+    const { locations, getLocations } = useContext(LocationContext)
     
     useEffect(() => {
         getLocations()
-    })
-    return locations
+    }, [])
+
+    return (
+        <div className="locations">
+        {
+            locations.map(location => <Location key={location.id} location={location} />)
+        }
+        </div>
+    )
 }
